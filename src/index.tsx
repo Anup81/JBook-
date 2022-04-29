b339c0f3b9f591e649414d145bcef25f5b2eb757
Anup81 <anupdhakal81@gmail.com>
2022-04-29 13:59:52 +0545

    JSX to Plain Javascript Transformation Done

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
-  const onClick = () => {
-    console.log(input);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    // console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  // Run this service one single time only mathi ko
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+    setCode(result.code);
   };
   return (
     <div>
